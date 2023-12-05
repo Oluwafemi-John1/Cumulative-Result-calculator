dc16196c3a257da2220d4b775a78879d96fc55f3
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-05 23:25:15 +0100

    work in progress

---
+++ script.js
@@ -0,0 +1,9 @@
+function calc() {
+    console.log(studentName.value, biology.value, physics.value, chemistry.value, math.value, geo.value, studentID.value, eng.value);
+
+    if (studentName.value == '' && biology.value == '' && physics.value == '' && chemistry.value == '' && math.value == '' && geo.value == '' && studentID.value == '' && eng.value == '') {
+        alert("Boda wa, haba")
+    } else {
+        alert("Go on bro")
+    }
+}
