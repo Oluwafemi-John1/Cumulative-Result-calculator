91175dd8ef69f83aa0611bdc1b1c4a1acedfa447
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-06 10:53:58 +0100

    Script in progress

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function calc() {
-    console.log(studentName.value, biology.value, physics.value, chemistry.value, math.value, geo.value, studentID.value, eng.value);
-
     if (studentName.value == '' && biology.value == '' && physics.value == '' && chemistry.value == '' && math.value == '' && geo.value == '' && studentID.value == '' && eng.value == '') {
         alert("Boda wa, haba")
     } else {
-        alert("Go on bro")
+        var confirmation = confirm("Are you sure you want to proceed?")
+        if (confirmation == true) {
+            var sum = Number(biology.value) + Number(physics.value) + Number(chemistry.value) + Number(math.value) + Number(geo.value) + Number(eng.value)
+            var cumu = ((sum/600) * 100) + "%"
+            console.log(cumu);
+        } else {
+            console.log(("check again"));
+        }
     }
 }
